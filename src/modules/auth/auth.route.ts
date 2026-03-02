c1466384481c09ac681da27c85469427af2758a2
Pablo Levy <pabloelevy@gmail.com>
2026-03-02 11:48:02 -0300

    protocol seed

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -9,6 +9,7 @@ import {
   logoutHandler,
 } from "./auth.controller.js";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import { requireRoles } from "../../middlewares/roles.middleware.js";
 
 export const authRoutes = Router();
 
@@ -17,6 +18,6 @@ authRoutes.post("/register", createUserHandler);
 authRoutes.get("/me", authMiddleware, meHandler);
 authRoutes.get("/me/bodegas", authMiddleware, meBodegasHandler);
 authRoutes.get("/me/roles", authMiddleware, meRolesHandler);
-authRoutes.post("/users", authMiddleware, createUserHandler);
+authRoutes.post("/users", authMiddleware, requireRoles(["super_admin"]), createUserHandler);
 authRoutes.post("/refresh", refreshHandler);
 authRoutes.post("/logout", logoutHandler);
